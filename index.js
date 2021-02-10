3cc13a79401008b13fca157f658cfead208f6000
Ayo-1 <52506241+Ayo-1@users.noreply.github.com>
2021-02-10 02:18:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const router = require('./routes/users')
 const questions = require('./routes/questions')
 const results = require('./routes/results')
-//app.use(cors())
+app.use(cors())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: false}))
 require('dotenv').config();
